ce189f0bff144cebe14176f848a2aa9bef55d666
Mourya <mourya.chiranjeevi@gmail.com>
2022-06-26 12:12:31 +0530

    refined code and added Posts redux Route yet to update the Route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AddPost from "./components/AddPost/AddPost";
 import PostsTable from "./components/PostsTable/PostsTable"
 import ErrorPage from "./ErrorPage";
 import PostDetails from "./components/PostDetails/PostDetails";
+import PostsRedux from "./components/PostsRedux/PostsRedux";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path = "/addpost" element = {<AddPost />} />
         <Route path="/posts/:id" element = {<PostDetails />} />
         <Route path="/poststable" element = {<PostsTable />} />
+        <Route path = "/postsredux" element = {<PostsRedux />} />
         <Route path="*" element = {<ErrorPage />} />
       </Routes>
       </div>
